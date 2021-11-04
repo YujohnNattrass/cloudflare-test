a8b9b4290489b19475ffc45fc7abe362930071d2
Yujohn Nattrass <yujohn.natt@gmail.com>
2021-11-04 12:22:28 -0700

    add gatsby icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from "react"
+import icon from '../images/icon.png';
 
 const IndexPage = () => {
   return (
     <main>
       <title>Home Page</title>
       <h1>Home page</h1>
+      <img src={icon} />
     </main>
   )
 }
